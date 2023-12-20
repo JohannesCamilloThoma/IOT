11cacdcab0edd6ccf7f92e9b4ceed5f1db705a93
JohannesCamilloThoma <110091475+JohannesCamilloThoma@users.noreply.github.com>
2023-12-20 09:50:33 +0100

    Update thingspeak.py

--- thingspeak.py
+++ thingspeak.py
@@ -23,6 +23,27 @@ while True:
     upload_to_thingspeak(value_to_upload)
     time.sleep(15)
 
+# Read last thingsspeak message
+def get_last_entry(channel_id, read_api_key, field_name='field1'):
+    url = f'https://api.thingspeak.com/channels/{channel_id}/feeds.json?api_key={read_api_key}&results=1'
+    
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Check for errors in the HTTP response
+
+        data = response.json()
+        if 'feeds' in data and data['feeds']:
+            last_entry = data['feeds'][0]
+            if field_name in last_entry:
+                return last_entry[field_name]
+            else:
+                print(f"Field '{field_name}' not found in the last entry.")
+        else:
+            print("No entries found in the channel.")
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+
+
 if __name__ == "__main__":
     pass
 
